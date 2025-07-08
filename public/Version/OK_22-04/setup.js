ecb71deb8b10b0d71e57683a10a52b7e9bd554d3
Enzio Isaia <enzioisaia@Mac-mini-di-Enzio.local>
2025-07-08 23:49:56 +0200

    Spiegazione dei file modificati

--- public/Version/OK_22-04/setup.js
+++ public/Version/OK_22-04/setup.js
@@ -3,7 +3,7 @@ const showName = "Na Famjia squasi normal";
 const showDate = "Sabato 02 Febbraio 2025";
 const imgIntest = "http://www.lacalzamaglia.it/wp-content/uploads/2024/06/logo-calza.png";
 const notespdf = "Questo documento non ha valore fiscale e non sostituisce il biglietto SIAE; esso è valido solo come documento attestante la prenotazione del posto per lo spettacolo indicato e dovrà essere presentato al momento dell'ingresso prima dell'inizio dello spettacolo e al contempo verrà sostituito dal biglietto SIAE. In caso di mancata presentazione non verrà riconosciuto alcun rimborso";
-const emailJsUserId = 'wgOAHSNswOPXrUPY7';
+// const emailJsUserId = 'wgOAHSNswOPXrUPY7';
 const apiUrl = 'http://localhost:3000';
 
 // Prezzi per zona
